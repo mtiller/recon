d924db655de6dfa8ed33e7450ea859494f9f7563
Michael Tiller <michael.tiller@gmail.com>
2013-11-13 21:31:03 -0500

    Adding code to handle meld format

--- recon/meld.py
+++ recon/meld.py
@@ -1,5 +1,35 @@
 class MeldWriter(object):
     def __init__(self, fp, verbose=False):
+        self.fp = fp
+        self.verbose = verbose
+    def _check_name(self, name):
+        pass
+    def add_table(self, name, signals):
+        return MeldTableWriter(self, name, signals)
+    def add_object(self, name):
+        return MeldObjectWriter(self, name)
+    def finalize(self):
+        pass
+    def close(self):
+        pass
+
+class MeldTableWriter(object):
+    def __init__(self, writer, name, signals):
+        self.writer = writer
+        self.name = name
+        self.signals = signals
+    def add_alias(self, alias, of, scale=1.0, offset=0.0):
+        pass
+    def write(self, name, data):
+        pass
+    def close(self):
+        pass
+
+class MeldObjectWriter(object):
+    def __init__(self, writer, name):
+        self.writer = writer
+        self.name = name
+    def write(self, **kwargs):
         pass
 
 class MeldReader(object):
