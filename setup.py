271f2dca51f109a207b566db2150ba77f13a3e98
Michael Tiller <michael.tiller@gmail.com>
2014-02-07 22:35:31 -0500

    Fixed up scripts and added setup script
    I added two scripts to the scripts directory, removed the .py from the scripts
    and then added a setup.py script.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from os.path import join, dirname
+
+setup(name="recon",
+      version="0.3.0",
+      description="Recon file format",
+      long_description=open(join(dirname(__file__), 'README.md')).read(),
+      author="Michael Tiller",
+      author_email="michael.tiller@gmail.com",
+      license="MIT",
+      url="http://github.com/xogeny/recon/",
+      scripts=['scripts/meld_info', 'scripts/wall_info',
+               'scripts/wall2meld', 'scripts/dsres2meld'],
+      packages=['recon'],
+      include_package_data=True,
+      zip_safe=False)
